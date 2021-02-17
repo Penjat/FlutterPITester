8952a631d134b10bf6fce847711df9bf247aad39
Penjat <theSpencerSymington@Gmail.com>
2021-02-16 19:13:26 -0800

    Created keypad.

--- lib/src/ui/Keypad.dart
+++ lib/src/ui/Keypad.dart
@@ -1,11 +1,62 @@
 import 'package:flutter/material.dart';
+
 class DigitKeypad extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 200.0,
-      height: 200.0,
-      color: Colors.blueGrey,
+      margin: const EdgeInsets.all(10.0),
+      color: Colors.amber[600],
+      child: Center(
+        child:
+        Column(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+          KeypadKey(0, pressedKey),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              KeypadKey(1, pressedKey),
+              KeypadKey(2, pressedKey),
+              KeypadKey(3, pressedKey),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              KeypadKey(4, pressedKey),
+              KeypadKey(5, pressedKey),
+              KeypadKey(6, pressedKey),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              KeypadKey(7, pressedKey),
+              KeypadKey(8, pressedKey),
+              KeypadKey(9, pressedKey),
+            ],
+          ),
+        ]),
+      ),
+    );
+  }
+  pressedKey(int keyNumber) {
+    print("pressed key " + keyNumber.toString());
+  }
+}
+
+class KeypadKey extends StatelessWidget {
+  KeypadKey(this.myNumber, this.pressedFunction);
+
+  Function pressedFunction;
+  int myNumber;
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton.extended(
+      onPressed: () {
+        pressedFunction(myNumber);
+      },
+      label: Text(myNumber.toString()),
+      backgroundColor: Colors.pink,
     );
   }
 }
