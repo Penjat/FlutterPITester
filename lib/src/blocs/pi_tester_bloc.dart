f0c5621336e781469896e67ee1d33075ba7c1b29
Penjat <theSpencerSymington@Gmail.com>
2021-02-16 16:31:33 -0800

    Created vc and vm.

---
+++ lib/src/blocs/pi_tester_bloc.dart
@@ -0,0 +1,7 @@
+// import '../resources/repository.dart';
+// import 'package:rxdart/rxdart.dart';
+// import '../models/item_model.dart';
+
+class PITesterBloc {
+
+}
---
+++ lib/src/ui/pi_tester_view_controller.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class PITesterViewController extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // bloc.fetchAllMovies();
+    return Text("I am a view controller");
+  }
+}
