995fa59342afa2a310022786b4ed6e47e1886060
Penjat <theSpencerSymington@Gmail.com>
2021-02-23 07:53:52 -0800

    Added style manaager.

--- lib/src/styles.dart
+++ lib/src/styles.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 
 class PiStyles {
-  static TextStyle correctDigitsStyle = TextStyle(fontSize: 20.0, color: Colors.amberAccent);
+  static TextStyle correctDigitsStyle = TextStyle(fontSize: 20.0, color: Colors.white60, fontWeight: FontWeight.bold);
+  static TextStyle mainTitle = TextStyle(fontSize: 25.0, color: Colors.white70, fontWeight: FontWeight.bold);
+  static TextStyle staticTitle = TextStyle(fontSize: 20.0, color: Colors.white38);
+  static TextStyle dynamicField = TextStyle(fontSize: 20.0, color: Colors.deepPurple);
+  static TextStyle keyPadTextStyle = TextStyle(fontSize: 14.0, color: Colors.white, fontWeight: FontWeight.bold);
+
+  static Color keyColor = Colors.purpleAccent;
+  static Color appBarColor = Colors.black12;
+  static Color flashDigitColor = Colors.deepPurpleAccent;
+
+  static ButtonStyle buttonStyle = ElevatedButton.styleFrom(
+  primary: Colors.black, // background
+  onPrimary: Colors.deepPurpleAccent, // foreground
+  );
 }
